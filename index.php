9874e1dc46ab26ff4bf2329e96709c9a4bfc24c5
michaellalastima <60122577+michaellalastima@users.noreply.github.com>
2020-12-17 21:24:47 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,61 @@
+<html>
+	<head>
+		<title>FINAL</title>
+			<link rel="stylesheet" type="text/css" href="css/style.css"/>
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+		<body>
+			<div class="wrapper">
+			<h3><nav role="navigation">
+			<div class="header"></div>
+					
+						  <ul><center>
+							<li><a href="Index.php">HOME</a></li>
+							<li><div class="CSS">CSS</div>
+							  <ul class="dropdown">
+								<li><a href="elements.php">Css Elements</a></li>
+								<li><a href="ta.php">Transitions & Animations</a></li>
+								<li><a href="ca.php">Css Animation</a></li>
+							  </ul>
+							</li>
+							<li><div class="SVG">SVG</div>
+							  <ul class="dropdown">
+								<li><a href="graphics.php">Intro to SVG</a></li>
+								<li><a href="sanim.php">SVG Animation</a></li>
+							  </ul>
+							<li><div class="CANVAS">CANVAS</div>
+							  <ul class="dropdown">
+								<li><a href="html5.php">Intro to HTML5 Canvas</a></li>
+							  </ul>
+							  <li><a href="about.php">ABOUT</a></li>
+						  </ul>
+						</nav></center>
+			<div class="clear"></div>
+			</div> 
+			<div class="box"><br>
+			<div class="words words-1">
+				  <span>C</span>
+				  <span>S</span>
+				  <span>S</span>
+				</div>
+				<div class="words words-1">
+				  <span>S</span>
+				  <span>V</span>
+				  <span>G</span>
+				</div>
+				<div class="words words-1">
+				  <span>C</span>
+				  <span>A</span>
+				  <span>N</span>
+				  <span>V</span>
+				  <span>A</span>
+				  <span>S</span>
+				</div>	
+				</div>
+				
+					<div class="element">
+					<center><br><br><br><a div class="fa fa-facebook">
+										<a div class="fa fa-twitter">
+										<a div class="fa fa-instagram">
+		</body>
+</html>
